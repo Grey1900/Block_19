2410533c364b8bd6f827e59ec3c7aa4fb11988d7
TimkyWimky <36457305+Grey1900@users.noreply.github.com>
2023-09-06 13:47:06 +0400

    end

--- Task_19_1/main.cpp
+++ Task_19_1/main.cpp
@@ -1,38 +1,92 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
+#include <vector>
 
-std::ifstream words;
-std::ifstream key_words;
 
-bool openTxt(void)
+bool openTxt(std::ifstream* words, std::ifstream* key_words)
 {
-    words.open("words.txt");
-    key_words.open("key_words.txt");
+    (*words).open("words.txt");
+    (*key_words).open("key_words.txt");
     
-    if(!words.is_open() || !key_words.is_open())
+    if(!(*words).is_open()) 
+        return false;
+    else if(!(*key_words).is_open())
         return false;
     
     return true;
 }
 
-void checkKeyWords(std::ifstream words, std::ifstream key_words)
-{   
+void keyWordsArray(std::ifstream* key_words, std::vector<std::string>& keyWordsArray)
+{  
+    std::string line;
 
+    while(std::getline(*key_words, line))
+    {
+        keyWordsArray.push_back(line);
+    }
+    (*key_words).close();
 }
 
-void outputResult(std::ifstream key_words)
+
+void checkKeyWords  (std::ifstream* words, 
+                    std::vector<std::string>& keyWordsArray)
+{   
+    std::string line = " ";
+    int count = 0;
+     
+    for(int i = 0; i < keyWordsArray.size(); i++)
+    {
+        while(std::getline((*words), line))
+        {
+            std::istringstream x(line);
+            std::string word;
+            while(x >> word)
+            {
+                if(word == keyWordsArray[i]){
+                    count++;
+                }
+            }
+        }
+        std::string result = keyWordsArray[i] + " = " + (std::to_string(count));
+        keyWordsArray.at(i) = result;
+        count = 0;
+    }
+}
+
+void outputResultToTxt(std::vector<std::string>& keyWords)
 {
-    
+    std::ofstream outputResult("key_words.txt");
+    if(outputResult.is_open()){
+        for(auto key : keyWords)
+        {
+            outputResult << key << std::endl;
+        }
+        outputResult.close();
+        std::cout << "Results written to key_words.txt" << std::endl;
+        system("key_words.txt");
+    } else {
+        std::cout << "Error opening file";
+    }
 }
 
 int main()
 {
-    std::cout   << "Opening files Words.txt and Key_Words.txt"
-                << std::endl;
-    std::cout   << (openTxt() ? 
+    std::ifstream words;
+    std::ifstream key_words;
+
+    std::cout   << "Opening files Words.txt and Key_Words.txt: ";
+                
+    std::cout   << (openTxt(&words, &key_words) ? 
                     "The files have been opened successfully":
                     "Error opening files")
                 << std::endl;
     
+    std::vector<std::string> keyWords;
+
+    keyWordsArray(&key_words, keyWords);
+    checkKeyWords(&words, keyWords);
+
+    outputResultToTxt(keyWords);
 }
