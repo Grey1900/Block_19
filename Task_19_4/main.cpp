dcbbe0546a79eeb79ed068940c21e9393103c034
TimkyWimky <email@email.com>
2023-09-27 11:06:36 +0400

    Task_19_4

--- Task_19_4/main.cpp
+++ Task_19_4/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <fstream>
- 
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+
+bool filePathAndOpen (std::ifstream& file, std::string& filePath)
+{
+    try
+    {
+        file.open(filePath, std::ios::binary);
+        if(!file.is_open())
+            throw std::runtime_error("Error opening file");
+        else
+            return true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr   << "Exception catch: "
+                    << e.what() 
+                    << std::endl;
+        return false;
+    }
+    
+}
+
+
+std::vector<unsigned char> signature (std::ifstream& file)
+{
+    std::vector<unsigned char> signature(8);
+    
+}
 
 int main()
 {
-    
+    std::ifstream file;
+    std::string pathFile;
+
+    std::cout   << "Enter the path of file: ";
+    std::cin    >> pathFile;
+
+    if(filePathAndOpen(file, pathFile)){
+        std::vector<unsigned char> signature{0x89, 'P', 'N', 'G'};
+    }
+
 }
